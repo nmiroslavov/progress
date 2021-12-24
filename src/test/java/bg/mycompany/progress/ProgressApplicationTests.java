1ae2b94530bf27b28481f0d1e71039a16dcc2abc
Nikola <n.miroslavov@protonmail.com>
2021-12-24 16:21:17 +0200

    Init project

---
+++ src/test/java/bg/mycompany/progress/ProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.mycompany.progress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
